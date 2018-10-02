a36a24c0448ebafc4a120568bc788bb1e505dc09
ZMH <zinminht3t@gmail.com>
2018-10-02 17:01:13 +0800

    Create generateMessage Utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
@@ -14,17 +15,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected!');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app!',
-    createAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined!',
-    createAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined!'));
 
   socket.on('disconnect', () => {
     console.log('User was Disconnected!');
@@ -32,11 +25,7 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log(`createMessage: `, message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
